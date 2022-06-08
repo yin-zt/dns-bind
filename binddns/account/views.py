778ad61dbba7042606a2f32d53471eb3626370c3
yin-zt <15626499421@163.com>
2022-06-08 09:01:19 +0800

    2. 增加用户登录验证功能

--- binddns/account/views.py
+++ binddns/account/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UserRegistrationForm
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -17,6 +19,28 @@ def user_login(request):
                     return HttpResponse('Disabled account')
             else:
                 return HttpResponse('Invalid login')
-        else:
-            form = LoginForm()
-        return render(request, 'account/login.html', {'form': form})
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            ## create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            #save the user object
+            new_user.save()
+            return render(request,'account/register_done.html',{'new_usesr': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,'account/register.html',{'user_form': user_form})
