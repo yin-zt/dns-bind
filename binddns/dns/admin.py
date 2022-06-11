7f919c43f0ef5c5543abef0bc8c51afb44e0be0e
yin-zt <15626499421@163.com>
2022-06-11 11:24:58 +0800

    增加了dns的数据库模型，并添加了数据信息

---
+++ binddns/dns/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import DNSRECORD
+
+@admin.register(DNSRECORD)
+class DNSRECORDAdmin(admin.ModelAdmin):
+    list_display = ['zone', 'domain', 'type', 'target', 'target_slave_flag', 'charge_person']
+    list_filter = ['zone', 'domain', 'charge_person']
+    list_editable = ['domain', 'target', 'charge_person']
+
---
+++ binddns/dns/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class DNSRECORD(models.Model):
+    TYPE_CHOICES = (
+        ('A', 'A'),
+        ('CNAME', 'CNAME'),
+        ('MX', 'MX'),
+        ('NS', 'NS'),
+        ('PTR', 'PTR')
+    )
+    target_flag_choice = (
+        ('master', "master"),
+        ("slave", "slave")
+    )
+    DNS_STATUS = (
+        ('online', 'online'),
+        ('offline', 'offline'),
+        ('maintain', "maintain")
+    )
+    domain = models.CharField(max_length=128, db_index=True)
+    view = models.CharField(max_length=128, blank=True)
+    zone = models.CharField(max_length=128, db_index=True)
+    type = models.CharField(max_length=128, choices=TYPE_CHOICES, default='A')
+    target = models.CharField(max_length=256, null=False)
+    target_slave = models.CharField(max_length=256, blank=True)
+    target_slave_flag = models.CharField(max_length=128, choices=target_flag_choice, default='master')
+    candidates_person = models.ForeignKey(User, on_delete=models.CASCADE, related_name="apply_dns")
+    charge_person = models.ForeignKey(User, on_delete=models.CASCADE, related_name="response_dns")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+        index_together = ('domain', 'zone')
+
+    def __str__(self):
+        return self.domain
